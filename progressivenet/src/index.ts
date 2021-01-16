4aef587320db7de38ba6dbbb0a1ad55af9f59a8e
Prev <prevdev@gmail.com>
2021-01-16 12:43:31 +0900

    Add demo

--- progressivenet/src/index.ts
+++
@@ -1,8 +0,0 @@
-export * as tf from '@tensorflow/tfjs';
-export { loadSequentially, ProgressiveNet } from './progressivenet';
-export { PGNetConfig, PGNetConfigLayerEntry } from './types';
-export { GraphModel } from './models/graph_model';
-export { LayersModel } from './models/layers_model';
-export { Classifier } from './frontend/classifier';
-export { ObjectDetector } from './frontend/detector';
-export * as quantization from './quantization';
